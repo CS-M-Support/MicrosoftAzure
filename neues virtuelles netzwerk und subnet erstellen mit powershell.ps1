ace1ad3edb3268f035707d1dddbdd6f4cc3f5ae5
Johnny Winkler <johnnyf.winkler@hotmail.com>
2022-04-27 11:24:24 +0200

    Create neues virtuelles netzwerk und subnet erstellen mit powershell.ps1

---
+++ neues virtuelles netzwerk und subnet erstellen mit powershell.ps1
@@ -0,0 +1,18 @@
+# Verbindung mit Azure Konto herstellen
+Connect-AzAccount
+
+# Neues virtuelles Netzwerk mit dem Address Prefix 10.0.0.0 in der Region Switzerland North und der Ressourcengruppe RG01 erstellen
+New-AzVirtualNetwork `
+    -Name 'VNet01' `
+    -ResourceGroupName 'RG01' `
+    -Location 'Switzerland Nort' `
+    -AddressPrefix '10.0.0.0/16'
+
+# Neues Subnetz zum virtuellen Netzwerk mit dem Subnet 24 und dem Namen default erstellen
+Add-AzVirtualNetworkSubnetConfig `
+    -Name 'default' `
+    -VirtualNetwork 'VNet01' `
+    -AddressPrefix '10.0.0.0/24'
+
+# Verbindnung mit Azure Konto trennen
+Disconnect-AzAccount
